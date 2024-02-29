b0d7929bc31ae668e35875a58d110e87114ad855
Deon Kouatchou <ddkouatchou@gmail.com>
2024-02-29 02:31:51 -0500

    Preliminary translation of src/posts/{data,endorsements}.js to TS

---
+++ src/posts/endorsements.ts
@@ -0,0 +1,52 @@
+import plugins from '../plugins';
+import { PostObject } from '../types';
+
+interface PostEndorsement {
+    post: PostObject;
+    isEndorsed: boolean;
+}
+
+interface PostsFunctions {
+    getPostFields: (pid: number, fields: string[]) => Promise<PostObject | null>;
+    setPostField: (pid: number, field: string, value: number) => Promise<void>;
+    endorse: (pid: number, uid: number) => Promise<PostEndorsement>;
+    unendorse: (pid: number, uid: number) => Promise<PostEndorsement>;
+}
+
+export default function(Posts: PostsFunctions) {
+    Posts.endorse = async function (pid: number, uid: number) {
+        return await toggleEndorsement('bookmark', pid, uid);
+    };
+
+    Posts.unendorse = async function (pid: number, uid: number) {
+        return await toggleEndorsement('unbookmark', pid, uid);
+    };
+
+    async function toggleEndorsement(type: string, pid: number, uid: number) {
+        const uidStr: string = uid.toString();
+        if (parseInt(uidStr, 10) <= 0) {
+            throw new Error('[[error:not-logged-in]]');
+        }
+
+        const isEndorsing: boolean = true;
+
+        const [postData]: PostObject[] = await Promise.all([
+            Posts.getPostFields(pid, ['pid', 'uid']),
+        ]);
+
+        await Posts.setPostField(pid, 'endorsed', 1);
+
+        plugins.hooks.fire(`action:post.endorse`, {
+            pid: pid,
+            uid: uid,
+            owner: postData.uid,
+            current: 'endorsed',
+        });
+
+
+        return {
+            post: postData,
+            isEndorsed: isEndorsing,
+        };
+    }
+}
